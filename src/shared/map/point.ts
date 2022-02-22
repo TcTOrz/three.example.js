57585dcfa2bdf5637bfbabd4d80d1d454e629e20
lijian <591466539@qq.com>
2022-02-22 10:08:16 +0800

    feat: add point circle

--- src/shared/map/point.ts
+++ src/shared/map/point.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Li Jian
  * @Date: 2022-02-14 09:39:47
- * @LastEditTime: 2022-02-17 14:35:46
+ * @LastEditTime: 2022-02-22 10:05:23
  * @LastEditors: Li Jian
  * @description: 点UI
  */
@@ -22,6 +22,7 @@ export default class Point implements PointInterface {
   draw() {
     const data = _.cloneDeep(this.data)
     const mercator = geoMercator()
+    // 画锥形体
     data.map((elem: any) => {
       const pos = mercator(elem.position)
       const height = 3
@@ -43,10 +44,36 @@ export default class Point implements PointInterface {
       mesh.type = 'point'
       mesh.userData = elem
       this.scene.add(mesh)
-      this.createTween(position, mesh, height)
+      this.createTweenFromCone(position, mesh, height)
+    })
+    // 画圆环
+    data.map((elem: any) => {
+      const pos = mercator(elem.position)
+      const position = {
+        x: pos[0],
+        y: -pos[1],
+        z: 2.21,
+      } as THREE.Vector3
+      let geometry: THREE.CircleBufferGeometry | THREE.RingBufferGeometry =
+        new THREE.CircleBufferGeometry(0.3, 200)
+      let material = new THREE.MeshPhongMaterial({ color: 0x90e0ef, side: THREE.DoubleSide })
+      let mesh: THREE.Mesh = new THREE.Mesh(geometry, material)
+      mesh.position.set(position.x, position.y, position.z)
+      this.scene.add(mesh)
+      geometry = new THREE.RingBufferGeometry(0.3, 0.5, 50)
+      material = new THREE.MeshPhongMaterial({
+        color: 0x90e0ef,
+        side: THREE.DoubleSide,
+        transparent: true,
+        opacity: 1,
+      })
+      mesh = new THREE.Mesh(geometry, material)
+      mesh.position.set(position.x, position.y, position.z)
+      this.createTweenFromCircle(mesh)
+      this.scene.add(mesh)
     })
   }
-  createTween(position: THREE.Vector3, mesh: THREE.Mesh, height: number) {
+  createTweenFromCone(position: THREE.Vector3, mesh: THREE.Mesh, height: number) {
     new TWEEN.Tween({
       x: position.x,
       y: position.y,
@@ -66,10 +93,10 @@ export default class Point implements PointInterface {
       .easing(TWEEN.Easing.Quadratic.InOut)
       .start()
       .onComplete(() => {
-        this.createTween1(position, mesh, height)
+        this.createTweenFromCone1(position, mesh, height)
       })
   }
-  createTween1(position: THREE.Vector3, mesh: THREE.Mesh, height: number) {
+  createTweenFromCone1(position: THREE.Vector3, mesh: THREE.Mesh, height: number) {
     new TWEEN.Tween({
       x: position.x,
       y: position.y,
@@ -89,7 +116,57 @@ export default class Point implements PointInterface {
       .easing(TWEEN.Easing.Quadratic.InOut)
       .start()
       .onComplete(() => {
-        this.createTween(position, mesh, height)
+        this.createTweenFromCone(position, mesh, height)
+      })
+  }
+  createTweenFromCircle(mesh: THREE.Mesh) {
+    new TWEEN.Tween({
+      x: mesh.scale.x,
+      y: mesh.scale.y,
+      z: mesh.scale.z,
+    })
+      .to(
+        {
+          x: mesh.scale.x * 1.3,
+          y: mesh.scale.y * 1.3,
+          z: mesh.scale.z * 1.3,
+        },
+        800
+      )
+      .onUpdate(object => {
+        mesh.scale.set(object.x, object.y, object.z)
+        // @ts-ignore
+        mesh.material.opacity = 1.5 - object.z
+      })
+      .easing(TWEEN.Easing.Quadratic.InOut)
+      .start()
+      .onComplete(() => {
+        this.createTweenFromCircle1(mesh)
+      })
+  }
+  createTweenFromCircle1(mesh: THREE.Mesh) {
+    new TWEEN.Tween({
+      x: mesh.scale.x,
+      y: mesh.scale.y,
+      z: mesh.scale.z,
+    })
+      .to(
+        {
+          x: 0.9,
+          y: 0.9,
+          z: 0.9,
+        },
+        500
+      )
+      .onUpdate(object => {
+        mesh.scale.set(object.x, object.y, object.z)
+        // @ts-ignore
+        mesh.material.opacity = 1
+      })
+      .easing(TWEEN.Easing.Quadratic.InOut)
+      .start()
+      .onComplete(() => {
+        this.createTweenFromCircle(mesh)
       })
   }
 }
