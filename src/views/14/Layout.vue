6d12184703cf83b027739af2ec3645e5ac3b4410
lijian <591466539@qq.com>
2022-03-04 14:36:08 +0800

    style: a bit header style

--- src/views/14/Layout.vue
+++ src/views/14/Layout.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: Li Jian
  * @Date: 2022-02-18 10:41:00
- * @LastEditTime: 2022-03-04 10:47:05
+ * @LastEditTime: 2022-03-04 14:34:04
  * @LastEditors: Li Jian
  * @Description: 第一级(地图)程序HTML布局
 -->
@@ -11,9 +11,7 @@ import * as echarts from 'echarts'
 
 const headername = ref('test header')
 const times = ref(0)
-// const changeHeadername = () => {
-//   headername.value = `test header: ${times.value++}`
-// }
+
 const theme = reactive({
   width: `${document.body.clientWidth}px`,
 })
@@ -63,42 +61,70 @@ onMounted(() => {
   #header
     .left
     .right
+      .container
+        #aIcon
+          <svg version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="20px" height="15px" xmlns="http://www.w3.org/2000/svg">
+            <g transform="matrix(1 0 0 1 -1580 -19 )">
+              <path d="M 20.0184135977337 0  C 20.4348441926346 0  20.7273371104816 0.193892045454546  20.8958923512748 0.581676136363637  C 21.064447592068 0.989346590909089  20.9950424929178 1.33735795454545  20.6876770538244 1.62571022727273  L 13 8.97869318181818  L 13 20.0454545454545  C 13.3555240793201 20.4630681818182  13.1621813031161 20.7563920454545  12.7754957507082 20.9254261363636  C 12.6466005665722 20.9751420454545  12.5226628895184 21  12.4036827195467 21  C 12.1359773371105 21  11.9128895184136 20.9055397727273  11.7344192634561 20.7166193181818  L 7.92705382436261 16.8984375  C 7.73866855524079 16.7095170454545  7.64447592067989 16.4857954545455  7.64447592067989 16.2272727272727  L 7.64447592067989 8.97869318181818  L 0.312322946175637 1.62571022727273  C 0.00495750708215297 1.33735795454545  -0.0644475920679887 0.989346590909089  0.104107648725212 0.581676136363637  C 0.272662889518414 0.193892045454546  0.565155807365439 0  0.981586402266289 0  L 20.0184135977337 0  Z " fill-rule="nonzero" fill="#a4d8fd" stroke="none" transform="matrix(1 0 0 1 1580 19 )" />
+            </g>
+          </svg>
+        #bIcon
+        #cIcon
+        #dIcon
+        #eIcon
   .content
-    #left
-    #right
+    //- #left
+    //- #right
 </template>
 
 <style lang="scss" scoped>
 $width: v-bind('theme.width');
 $header-height: 50px;
-$side-width: 300px;
+$side-width: 150px;
+$opacity: 0.9;
 .layout {
-  position: absolute;
   top: 0;
   left: 0;
-  * {
-    opacity: 0.8;
+  & > div {
+    opacity: $opacity;
   }
   #header {
     width: $width;
     height: $header-height;
     background-image: url('@assets/image/u6.png');
     background-repeat: no-repeat;
-    background-size: 100%;
+    background-size: 100% 100%;
+    display: flex;
+    justify-content: space-between;
     .left {
-      width: 100px;
+      width: 80px;
+      height: 50px;
+      background-image: url('@assets/image/menu_u240.png');
+      background-repeat: no-repeat;
+      background-size: 50px;
+      background-position: center;
+      opacity: 1;
+      cursor: pointer;
     }
     .right {
       width: 100px;
+      height: 40px;
+      .container {
+        width: 100%;
+        height: 100%;
+        & > div {
+          display: flex;
+          align-items: center;
+        }
+        #aIcon {
+          width: 20px;
+          height: 100%;
+        }
+      }
     }
   }
   .content {
     top: $header-height;
-    // height: calc(100vh - $header-height);
-
-    // width: $side-width;
-    // display: flex;
-    // justify-content: space-between;
     #left {
       width: $side-width;
       height: calc(100vh - $header-height);
